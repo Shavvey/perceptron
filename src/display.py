89d3e0b7309ce8bb465a730d35db7c806b6ff8fb
Cole Johnson <coledjohn@gmail.com>
2026-05-05 02:51:50 -0600

    Fixing train algorithm

--- src/display.py
+++ src/display.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from perceptron import Perceptron
 from pandas import DataFrame
+import numpy as np
 
 
 def display(p: Perceptron, points: DataFrame):
@@ -8,9 +9,19 @@ def display(p: Perceptron, points: DataFrame):
     xs: list[float] = []
     ys: list[float] = []
     labels: list[int] = []
-    for x1, x2, label, in zip(points["x1"], points["x2"], points["label"]):
+    for (
+        x1,
+        x2,
+        label,
+    ) in zip(points["x1"], points["x2"], points["label"]):
         xs.append(x1)
         ys.append(x2)
         labels.append(label)
     axes.scatter(xs, ys, c=labels)
+
+    # graph perceptron linear separation line
+    xss = np.linspace(0, 1, 10)
+    yss = -p.weights[0] / p.weights[1] * xss - (p.bias / p.weights[1])
+
+    axes.plot(xss, yss)
     plt.show()
